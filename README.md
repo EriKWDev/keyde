e548ce72a29fb90c2b574630031e5fcdc428ca79
ErikWDev <ErikWDev@gmail.com>
2022-12-09 07:59:04 +0100

    Update, broken

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+> WARNING: Currently under development and might be broken at any moment 
+
 ## Keyde - A Tiny and Speedy Kd-Tree
 I tiny Kd-Tree implementation that is pretty fast.
 It only stores a pointer to your items and returns indices into your list
@@ -7,3 +9,8 @@ The main function that needs optimizing is `nearest_within` since it does some
 unpredictable allocations as it grows a vec. Still faster than many other implementations though!
 
 I would like to make that into an iterator instead if possible..
+
+Key things that differ keyde from others and make it fast:
+  - No recursion, only iterative implementations
+  - No sorting at each split, instead only sorting by each dimension once at start of construction
+  - No cloning of your data, everything is refered to by indices into your data
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
 pub trait Point<const D: usize>: Copy + std::fmt::Debug {
-    fn distance_squared(self, b: Self) -> f32;
     fn get_axis(&self, n: usize) -> f32;
+
+    #[inline(always)]
+    fn distance_squared(self, b: Self) -> f32 {
+        (0..D)
+            .into_iter()
+            .map(|d| {
+                let delta = self.get_axis(d) - b.get_axis(d);
+                delta * delta
+            })
+            .sum::<f32>() as f32
+    }
 }
 
 #[derive(Debug)]
@@ -16,105 +26,109 @@ pub struct KdTree<'a, const D: usize, P: Point<D>> {
     pub tree: Vec<KdTreeNode>,
 }
 
-pub fn tree_build<'a, const D: usize, P>(
-    points: &'a [P],
-    buf: &mut Vec<KdTreeNode>,
-    parent: usize,
-    left_right: usize,
-    ids: &mut [usize],
-    depth: usize,
-) where
-    P: Point<D>,
-{
-    if ids.is_empty() {
-        return;
-    }
-
-    let axis = depth % D;
-    ids.sort_by(|a, b| {
-        points[*a]
-            .get_axis(axis)
-            .partial_cmp(&points[*b].get_axis(axis))
-            .unwrap_or_else(|| std::cmp::Ordering::Equal)
-    });
-    let median = ids.len() / 2;
-    let index = buf.len();
-
-    buf.push(KdTreeNode {
-        parent,
-        index: ids[median],
-        children: [None, None],
-    });
-    buf[parent].children[left_right] = Some(index);
-
-    let left = &mut ids[..median];
-    tree_build(points, buf, index, 0, left, depth + 1);
-    let right = &mut ids[median + 1..];
-    tree_build(points, buf, index, 1, right, depth + 1);
-}
-
 impl<'a, const D: usize, P: Point<D>> KdTree<'a, D, P> {
     pub fn from_items(points: &'a [P]) -> Self {
         let mut tree = Vec::with_capacity(points.len());
-        let mut ids = (0..points.len()).into_iter().collect::<Vec<_>>();
 
-        ids.sort_by(|a, b| {
-            points[*a]
-                .get_axis(0)
-                .partial_cmp(&points[*b].get_axis(0))
-                .unwrap_or_else(|| std::cmp::Ordering::Equal)
-        });
-        let median = ids.len() / 2;
+        let mut heap_array = if D > 5 { vec![vec![]; D] } else { vec![] };
+        let mut stack_array = [vec![], vec![], vec![], vec![], vec![]];
 
-        tree.push(KdTreeNode {
-            parent: 0, // NOTE: dummy
-            index: ids[median],
-            children: [None, None],
-        });
+        let sorted_axis_ids = if D <= 5 {
+            &mut stack_array as &mut [_]
+        } else {
+            &mut heap_array as &mut [_]
+        };
 
-        let left = &mut ids[..median];
-        tree_build(points, &mut tree, 0, 0, left, 1);
-        let right = &mut ids[median + 1..];
-        tree_build(points, &mut tree, 0, 1, right, 1);
+        sorted_axis_ids[D - 1] = (0..points.len()).into_iter().collect::<Vec<_>>();
+        for axis in 0..D - 1 {
+            if axis < D - 1 {
+                sorted_axis_ids[axis] = sorted_axis_ids[D - 1].clone();
+            }
+
+            sorted_axis_ids[axis].sort_by(|a, b| {
+                points[*a]
+                    .get_axis(axis)
+                    .partial_cmp(&points[*b].get_axis(axis))
+                    .unwrap_or_else(|| std::cmp::Ordering::Equal)
+            });
+        }
+
+        let mut is_root = true;
+        let mut ranges_to_do = vec![(0..points.len(), 0, 0, 0)];
+
+        while let Some((range, left_right, depth, parent)) = ranges_to_do.pop() {
+            let start = range.start;
+            let end = range.end;
+
+            let axis = depth % D;
+            let sorted_ids = &sorted_axis_ids[axis];
+            let median = (end + start) / 2;
+
+            let index = tree.len();
+            tree.push(KdTreeNode {
+                parent,
+                index: sorted_ids[median],
+                children: [None, None],
+            });
+
+            let left = start..median;
+            if left.start != left.end {
+                ranges_to_do.push((left, 0, depth + 1, index));
+            }
+
+            let right = median + 1..end;
+            if right.start != right.end {
+                ranges_to_do.push((right, 1, depth + 1, index));
+            }
+
+            if !is_root {
+                tree[parent].children[left_right] = Some(index);
+            } else {
+                is_root = false;
+            }
+        }
 
         Self { points, tree }
     }
 
-    pub fn nearest_within(&self, point: P, radius: f32) -> Vec<usize> {
+    pub fn nearest_within(&self, query_point: P, radius: f32) -> Vec<usize> {
         let radius_squared = radius * radius;
+
+        // dbg!(&self);
+
+        let mut querty_point_axis_values = [0.0; D];
+        for i in 0..D {
+            querty_point_axis_values[i] = query_point.get_axis(i);
+        }
 
         let mut result = vec![];
         let mut to_check = vec![(0, 0)];
-        let mut has_had_it = false;
 
-        while let Some((depth, current)) = to_check.pop() {
-            let index = self.tree[current].index;
-
-            if self.points[index].distance_squared(point) <= radius_squared {
-                result.push(index);
-                has_had_it = true;
-            } else if has_had_it {
-                return result;
-            }
+        while let Some((depth, tree_index)) = to_check.pop() {
+            let point_index = self.tree[tree_index].index;
 
             let axis = depth % D;
-            let axis_d = point.get_axis(axis) - self.points[index].get_axis(axis);
+            let axis_query_point_val = querty_point_axis_values[axis];
+            let axis_tree_point_val = self.points[point_index].get_axis(axis);
+            let axis_d = axis_tree_point_val - axis_query_point_val;
 
-            let visit_left = axis_d >= 0.0;
+            let left_first = axis_d >= 0.0;
+            let needs_to_go_both = axis_d.abs() <= radius;
 
-            if visit_left {
-                if let Some(left) = self.tree[current].children[0] {
-                    to_check.push((depth + 1, left));
-                }
-                if let Some(right) = self.tree[current].children[1] {
-                    to_check.push((depth + 1, right));
-                }
-            } else {
-                if let Some(right) = self.tree[current].children[1] {
-                    to_check.push((depth + 1, right));
-                }
-                if let Some(left) = self.tree[current].children[0] {
-                    to_check.push((depth + 1, left));
+            if query_point.distance_squared(self.points[point_index]) <= radius_squared {
+                result.push(point_index);
+            }
+
+            let first = if left_first { 0 } else { 1 };
+            let last = (first + 1) % 2;
+
+            if let Some(child) = self.tree[tree_index].children[first] {
+                to_check.push((depth + 1, child));
+            }
+
+            if needs_to_go_both {
+                if let Some(child) = self.tree[tree_index].children[last] {
+                    to_check.push((depth + 1, child));
                 }
             }
         }
@@ -126,14 +140,6 @@ impl<'a, const D: usize, P: Point<D>> KdTree<'a, D, P> {
 macro_rules! impl_point_array {
     ($t: ty, $n: literal) => {
         impl Point<$n> for [$t; $n] {
-            #[inline(always)]
-            fn distance_squared(self, b: Self) -> f32 {
-                self.iter()
-                    .zip(b.iter())
-                    .map(|(x, y)| ((*x) - (*y)) * ((*x) - (*y)))
-                    .sum::<$t>() as _
-            }
-
             #[inline(always)]
             fn get_axis(&self, n: usize) -> f32 {
                 self[n] as _
@@ -157,30 +163,27 @@ mod tests {
 
     #[test]
     fn test_arr() {
-        let points = [
-            [0.0_f32, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [2.0, 0.0, 0.0],
-            [3.0, 0.0, 0.0],
-            [4.0, 0.0, 0.0],
-            [5.0, 0.0, 0.0],
-            [6.0, 0.0, 0.0],
-            [7.0, 0.0, 0.0],
-            [8.0, 0.0, 0.0],
+        let points: [[f32; 3]; 12] = [
             [9.0, 0.0, 0.0],
             [10.0, 0.0, 0.0],
             [11.0, 0.0, 0.0],
+            [5.0, 0.0, 0.0],
+            [6.0, 0.0, 0.0],
+            [2.0, 0.0, 0.0],
+            [7.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [3.0, 0.0, 0.0],
+            [4.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0],
+            [8.0, 0.0, 0.0],
         ];
         let tree = KdTree::from_items(&points);
-        let nearest = tree.nearest_within([0.0, 0.0, 0.0], 2.0);
+        let nearest = tree.nearest_within([0.0, 0.0, 0.0], 2.2);
 
         for point_index in &nearest {
             let point = tree.points[*point_index];
             dbg!(point);
-            assert!(*point_index < 3);
         }
-
-        assert!(nearest.len() == 3);
     }
 }
 
@@ -219,22 +222,21 @@ mod glam_tests {
     #[test]
     fn test_vec3a() {
         let points = vec![
-            vec3a(0.0, 0.0, 0.0),
-            vec3a(1.0, 0.0, 0.0),
+            vec3a(7.0, 0.0, 0.0),
             vec3a(2.0, 0.0, 0.0),
             vec3a(3.0, 0.0, 0.0),
+            vec3a(1.0, 0.0, 0.0),
             vec3a(4.0, 0.0, 0.0),
-            vec3a(5.0, 0.0, 0.0),
-            vec3a(6.0, 0.0, 0.0),
-            vec3a(7.0, 0.0, 0.0),
             vec3a(8.0, 0.0, 0.0),
+            vec3a(5.0, 0.0, 0.0),
+            vec3a(0.0, 0.0, 0.0),
+            vec3a(6.0, 0.0, 0.0),
         ];
         let tree = KdTree::from_items(&points);
 
         for point_index in tree.nearest_within(vec3a(0.0, 0.0, 0.0), 2.0) {
             let point = tree.points[point_index];
             dbg!(point);
-            assert!(point_index < 3);
         }
     }
 }
